3fde2dd252d773c821f3f31767e1c096f63cf4e3
FeliciousX <feliciousx@gmail.com>
2017-10-17 01:10:36 +0800

    fix(nvm): path to use proper home env

--- oh-my-zsh/nodejs.zsh
+++ oh-my-zsh/nodejs.zsh
@@ -1,5 +1,5 @@
 # Using NVM to manage nodejs versions
 # https://github.com/creationix/nvm
-export NVM_DIR="/Users/feliciousx/.nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
