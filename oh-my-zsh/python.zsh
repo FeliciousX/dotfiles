1693d17b41a56b5ec215c91d350c0e1e95a73300
FeliciousX <feliciousx@gmail.com>
2018-01-19 16:19:34 +0800

    feat(zsh): add python

---
+++ oh-my-zsh/python.zsh
@@ -0,0 +1,6 @@
+export WORKON_HOME=~/.virtualenvs
+#export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
+#export VIRTUALENVWRAPPER_VIRTUALENV=/usr/bin/virtualenv2
+source /usr/bin/virtualenvwrapper_lazy.sh
+#source /usr/bin/virtualenvwrapper.sh
+export PATH="$HOME/.local/bin:$PATH"
