f3b0eb082208f2584046a06f54e2f2e8f9864b56
FeliciousX <feliciousx@gmail.com>
2018-01-19 16:19:24 +0800

    feat(zsh): update ruby add path

--- oh-my-zsh/ruby.zsh
+++ oh-my-zsh/ruby.zsh
@@ -1,5 +1,9 @@
 # Use RVM to manage ruby versions
 # https://rvm.io/
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+
 # Load RVM into a shell session *as a function*
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
